31dc31b4fe3838a9238226a6ccd682d04fdee813
Rohan Koodli <rovik05@gmail.com>
2020-10-22 15:53:52 -0700

    try

--- temp.py
+++ temp.py
@@ -1,13 +1,15 @@
 import ray
 from ray import serve
 import tensorflow as tf
+import numpy as np
+from tqdm import tqdm
 
 import epitome.generators as generators
 import epitome.functions as functions
 from epitome.constants import *
 from epitome.models import VLP
+from epitome import *
 
-import numpy as np
 import os
 
 import itertools
@@ -16,53 +18,73 @@ def gen():
     for i in itertools.count(10):
         yield (i, [1] * i)
 
-class A:
-    def __init__(self):
-        model = VLP(['CEBPB'], test_celltypes=['K562'])
-        self.model = model
-        self.accessilibility_peak_matrix = np.random.rand(4, 10)
+class A(VLP):
+    def __init__(self, accessilibility_peak_matrix, regions_peak_file):
+        VLP.__init__(self, assays=['CEBPB'], test_celltypes=['K562'])
+        # self.model = model
+        self.accessilibility_peak_matrix = accessilibility_peak_matrix
+        self.regions_peak_file = regions_peak_file
+
     
-    def func2(self, num, all_data, all_data_regions, idx, joined):
-        peaks_i = np.zeros((len(all_data_regions)))
-        peaks_i[idx] = self.accessilibility_peak_matrix[0, joined['idx']]
-        load_bs = generators.load_data(all_data,
-                    self.model.test_celltypes,   # used for labels. Should be all for train/eval and subset for test
-                    self.model.eval_cell_types,   # used for rotating features. Should be all - test for train/eval
-                    self.model.matrix,
-                    self.model.assaymap,
-                    self.model.cellmap,
-                    radii = self.model.radii,
+    def func2(self, data, matrix, indices, samples = 50):
+        # peaks_i = np.zeros((len(all_data_regions)))
+        # peaks_i[idx] = self.accessilibility_peak_matrix[0, joined['idx']]
+        load_bs = generators.load_data(data,
+                    self.test_celltypes,   # used for labels. Should be all for train/eval and subset for test
+                    self.eval_cell_types,   # used for rotating features. Should be all - test for train/eval
+                    self.matrix,
+                    self.assaymap,
+                    self.cellmap,
+                    radii = self.radii,
                     mode = Dataset.RUNTIME,
-                    similarity_matrix = peaks_i,
-                    similarity_assays = self.model.similarity_assays,
-                    indices = idx)
+                    similarity_matrix = matrix,
+                    similarity_assays = self.similarity_assays,
+                    indices = indices)
+        num_samples = len(indices)
         print('done loading')
-        input_shapes, output_shape, v = generators.generator_to_tf_dataset(load_bs, self.model.batch_size, 1, self.model.prefetch_size)
+        input_shapes, output_shape, v = generators.generator_to_tf_dataset(load_bs, self.batch_size, 1, self.prefetch_size)
         print('done generating')
         dataset = tf.data.Dataset.range(4)
         dataset = tf.data.Dataset.from_generator(
         gen,
         (tf.int64, tf.int64),
         (tf.TensorShape([]), tf.TensorShape([None])))
-        # dataset = dataset.batch(model.batch_size)
-        # dataset = dataset.shuffle(model.shuffle_size)
-        # dataset = dataset.repeat()
-        # dataset = dataset.prefetch(model.prefetch_size)
         print('before loop')
         for f in v.take(2):
             print('in loop')
-        return 2 * num
+        return 2
 
     @serve.accept_batch
-    def __call__(self, requests):
-        all_data_regions = functions.bed2Pyranges(self.model.regionsFile)
+    def __call__(self, data, matrix, indices):
+        input_shapes, output_shape, ds = functions.generator_to_tf_dataset(generators.load_data(data,
+                 self.test_celltypes,   # used for labels. Should be all for train/eval and subset for test
+                 self.eval_cell_types,   # used for rotating features. Should be all - test for train/eval
+                 self.matrix,
+                 self.assaymap,
+                 self.cellmap,
+                 radii = self.radii,
+                 mode = Dataset.RUNTIME,
+                 similarity_matrix = matrix,
+                 similarity_assays = self.similarity_assays,
+                 indices = indices), self.batch_size, 1, self.prefetch_size)
+
+        num_samples = len(indices)
+
+        # results = self.run_predictions(num_samples, ds, calculate_metrics = False)
+
+        # return [results['preds_mean']]
+        return [69]
+
+    # @serve.accept_batch
+    def old_call(self, requests):
+        all_data_regions = functions.bed2Pyranges(self.regionsFile)
         for req in requests:
             regions_peak_file = req.data[1]
             regions_bed = functions.bed2Pyranges(regions_peak_file)
             
             joined = regions_bed.join(all_data_regions, how='left',suffix='_alldata').df
             idx = joined['idx_alldata']
-            all_data = functions.concatenate_all_data(self.model.data, self.model.regionsFile)
+            all_data = functions.concatenate_all_data(self.data, self.regionsFile)
             # print(req.data[1]) # test if method is entered
             arg = 42
             answer = self.func2(arg, all_data, all_data_regions, idx, joined)
@@ -72,9 +94,12 @@ class A:
         
         # do stuff, serve model
     
-    def score_matrix(self, x, y):
+    def score_matrix(self, regions_indices = None, all_data = None):
         client = serve.start()
         client.create_backend("tf", A,
+            # init args
+            self.accessilibility_peak_matrix,
+            self.regions_peak_file, 
             # configure resources
             ray_actor_options={"num_cpus": 2},
             # configure replicas
@@ -87,18 +112,39 @@ class A:
         client.create_endpoint("tf", backend="tf")
         handle = client.get_handle("tf")
 
-        regions_peak_file = os.getcwd() + '/data/test_regions.bed'
-        args = [(1, regions_peak_file), (2, regions_peak_file)]
+        if all_data is None:
+            all_data = functions.concatenate_all_data(self.data, self.regionsFile)
 
-        # futures = [handle.remote(i) for i in args]
+        regions_bed = functions.bed2Pyranges(self.regions_peak_file)
+        all_data_regions = functions.bed2Pyranges(self.regionsFile)
+
+        joined = regions_bed.join(all_data_regions, how='left',suffix='_alldata').df
+
+        # select regions with data to score
+        if regions_indices is not None:
+            joined = joined[joined['idx'].isin(regions_indices)]
+            joined = joined.reset_index()
+        
+        idx = joined['idx_alldata']
+
+        args = [(1, self.regions_peak_file), (2, self.regions_peak_file)]
+
+        # # futures = [handle.remote(i) for i in args]
         futures = []
-        for i in args:
-            # j = i + 1
-            futures.append(handle.remote(i))
+        for sample_i in tqdm(range(self.accessilibility_peak_matrix.shape[0])):
+            peaks_i = np.zeros((len(all_data_regions)))
+            peaks_i[idx] = self.accessilibility_peak_matrix[sample_i, joined['idx']]
+            futures.append(handle.remote((all_data, peaks_i, idx)))
         
         result = ray.get(futures)
         return result
+    
+    def test(self):
+        print(self.eval_vector)
+        print(self.regionsFile)
 
 if __name__ == '__main__':
-    a = A()
-    print(a.score_matrix(1, 2))
+    a = A(accessilibility_peak_matrix=np.random.rand(4, 10), regions_peak_file=os.getcwd() + '/data/test_regions.bed')
+    a.score_matrix()
+
+    # print(a.score_matrix(1, 2))
