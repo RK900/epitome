c51c026e54e68849e0d36301bd8b227db0ec9e3e
Rohan Koodli <rovik05@gmail.com>
2021-02-05 18:43:48 -0800

    stuff

--- temp2.py
+++ temp2.py
@@ -11,13 +11,12 @@ from epitome.models import VLP
 from epitome import *
 
 import os
+import sys
 import copy
 
 import itertools
 import psutil
 import gc
-
-ray.init(num_cpus=48)
 
 def auto_garbage_collect(pct=50.0):
     if psutil.virtual_memory().percent >= pct:
@@ -83,12 +82,11 @@ class A(VLP):
                 indices = indices), self.batch_size, 1, self.prefetch_size)
 
         num_samples = len(indices)
-        # auto_garbage_collect()
+
         results = self.run_predictions(num_samples, ds, calculate_metrics = False)
-        # auto_garbage_collect()
+
 
         return [results['preds_mean']]
-        # return [69]
 
     # @serve.accept_batch
     def old_call(self, requests):
@@ -171,7 +169,10 @@ class A(VLP):
         print(self.regionsFile)
 
 if __name__ == '__main__':
-    apm = np.ones((10_000, 130_000))
+    ray.init(num_cpus=48)
+    # os.system('ray start --object-store-memory 355*10**9')
+
+    apm = np.ones((25, 1000))
     rpf = os.getcwd() + '/data/test_regions.bed'
 
     metadata_class = VLP( assays=['CEBPB', "JUN", 'TCF7', 'CEBPZ'])
@@ -179,9 +180,7 @@ if __name__ == '__main__':
     all_data = functions.concatenate_all_data(metadata_class.data, metadata_class.regionsFile)
 
     # a = A.remote(accessilibility_peak_matrix=apm, regions_peak_file=rpf, all_data=all_data)
-    metadata_class = VLP( assays=['CEBPB', "JUN", 'TCF7', 'CEBPZ'])
-    regionsFile = metadata_class.regionsFile
-    all_data = functions.concatenate_all_data(metadata_class.data, metadata_class.regionsFile)
+    
     # a.test.remote()
     # t = ray.get(a.predict_step.remote((1, [1, 2, 3])))
 
@@ -202,7 +201,7 @@ if __name__ == '__main__':
     # # futures = [handle.remote(i) for i in args]
     futures = []
     results = []
-    num_classes = 1
+    num_classes = 5
     classes = [A.remote(accessilibility_peak_matrix=apm, regions_peak_file=rpf, all_data=all_data) for i in range(num_classes)]
     [c.train.remote(1000) for c in classes]
 
@@ -211,6 +210,7 @@ if __name__ == '__main__':
         peaks_i = np.zeros((len(all_data_regions)))
         peaks_i[idx] = apm[sample_i, joined['idx']]
         value = classes[sample_i % num_classes].eval_vector.remote((peaks_i, idx))
+        print("ray thing size", sys.getsizeof(value))
         futures += [value]
         if sample_i % 1500 == 0 and sample_i > 0:
             print("Clearing object store")
